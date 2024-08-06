ae25648e68137291361cc203e66e1b3f45bcaf96
Nesrux <joaomarcosdevs@gmail.com>
2024-08-06 17:51:29 -0300

    feat: cria os beans de usecase de categorias

--- application/src/test/java/com/nesrux/catalogo/application/application/category/delete/DeleteCategoryUseCaseTest.java
+++ application/src/test/java/com/nesrux/catalogo/application/application/category/delete/DeleteCategoryUseCaseTest.java
@@ -1,49 +1,50 @@
 package com.nesrux.catalogo.application.application.category.delete;
 
+import com.nesrux.catalogo.application.application.UseCaseTest;
+import com.nesrux.catalogo.application.category.delete.DeleteCategoryUseCase;
 import com.nesrux.catalogo.domain.Fixture;
 import com.nesrux.catalogo.domain.category.CategoryGateway;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
-public class DeleteCategoryUseCaseTest {
+public class DeleteCategoryUseCaseTest extends UseCaseTest {
+
     @InjectMocks
     private DeleteCategoryUseCase useCase;
 
     @Mock
     private CategoryGateway categoryGateway;
 
-
+    @Test
     public void givenValidId_whenCallsDelete_shouldBeOk() {
-        //given
+        // given
         final var aulas = Fixture.Categories.aulas();
         final var expectedId = aulas.id();
 
         doNothing()
                 .when(this.categoryGateway).deleteById(anyString());
 
-        //when
-        Assertions.assertDoesNotThrow(() -> this.categoryGateway.deleteById(expectedId));
+        // when
+        Assertions.assertDoesNotThrow(() -> this.useCase.execute(expectedId));
 
-        //then
-        verify(this.categoryGateway, times(1))
-                .deleteById(expectedId);
+        // then
+        verify(this.categoryGateway, times(1)).deleteById(eq(expectedId));
     }
 
+    @Test
     public void givenInvalidId_whenCallsDelete_shouldBeOk() {
-        //given
+        // given
         final String expectedId = null;
 
-        //when
-        Assertions.assertDoesNotThrow(() -> this.categoryGateway.deleteById(expectedId));
+        // when
+        Assertions.assertDoesNotThrow(() -> this.useCase.execute(expectedId));
 
-        //then
-        verify(this.categoryGateway, never())
-                .deleteById(expectedId);
+        // then
+        verify(this.categoryGateway, never()).deleteById(eq(expectedId));
     }
 }
