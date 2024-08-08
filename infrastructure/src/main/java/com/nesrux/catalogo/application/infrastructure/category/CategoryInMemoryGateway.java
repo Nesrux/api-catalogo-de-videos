a9cd266767910d4c7386ed0e7791920e7e63e77c
Nesrux <joaomarcosdevs@gmail.com>
2024-08-08 16:40:13 -0300

    feat: diciona uma implementação em memoria do categorygatewayã

---
+++ infrastructure/src/main/java/com/nesrux/catalogo/application/infrastructure/category/CategoryInMemoryGateway.java
@@ -0,0 +1,46 @@
+package com.nesrux.catalogo.application.infrastructure.category;
+
+import com.nesrux.catalogo.domain.category.Category;
+import com.nesrux.catalogo.domain.category.CategoryGateway;
+import com.nesrux.catalogo.domain.category.CategorySearchQuery;
+import com.nesrux.catalogo.domain.pagination.Pagination;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class CategoryInMemoryGateway implements CategoryGateway {
+    private final ConcurrentHashMap<String, Category> db;
+
+    public CategoryInMemoryGateway() {
+        this.db = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public Category save(final Category aCategory) {
+        this.db.put(aCategory.id(), aCategory);
+        return aCategory;
+    }
+
+    @Override
+    public void deleteById(final String anId) {
+        this.db.remove(anId);
+    }
+
+    @Override
+    public Optional<Category> findById(String anId) {
+        return Optional.ofNullable(this.db.get(anId));
+    }
+
+    @Override
+    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
+        final var values = this.db.values();
+        return new Pagination<>(
+                aQuery.page(),
+                aQuery.perPage(),
+                values.size(),
+                new ArrayList<>(values));
+    }
+}
