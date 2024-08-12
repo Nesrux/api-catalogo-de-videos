1df522db6a73df1f722e5f65d64c045d9a19fb12
Nesrux <joaomarcosdevs@gmail.com>
2024-08-12 14:24:07 -0300

    refactor: melhorando os imports das annotations

--- infrastructure/src/main/java/com/nesrux/catalogo/infrastructure/configuration/ObjectMapperConfig.java
+++ infrastructure/src/main/java/com/nesrux/catalogo/infrastructure/configuration/ObjectMapperConfig.java
@@ -2,13 +2,13 @@ package com.nesrux.catalogo.infrastructure.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nesrux.catalogo.infrastructure.configuration.json.Json;
+import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@JsonComponent
 public class ObjectMapperConfig {
     @Bean
-    public ObjectMapper mapper(){
+    public ObjectMapper mapper() {
         return Json.mapper();
     }
 }
--- infrastructure/src/test/java/com/nesrux/catalogo/infrastructure/JacksonTest.java
+++ infrastructure/src/test/java/com/nesrux/catalogo/infrastructure/JacksonTest.java
@@ -1,10 +1,6 @@
 package com.nesrux.catalogo.infrastructure;
 
-import com.nesrux.catalogo.infrastructure.configuration.ObjectMapperConfig;
 import org.junit.jupiter.api.Tag;
-import org.springframework.boot.test.autoconfigure.json.JsonTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
@@ -14,8 +10,5 @@ import java.lang.annotation.*;
 @Inherited
 @ActiveProfiles("test-integration")
 @Tag("integrationTest")
-@JsonTest(includeFilters = {
-        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = ObjectMapperConfig.class)
-})
 public @interface JacksonTest {
 }
