2a49ed2c7a7bcdcbf07d6b8ea80a2fe0bfd616c7
Nesrux <joaomarcosdevs@gmail.com>
2024-08-14 21:04:34 -0300

    feat: cria configuração de elasticsearch  para test containers

---
+++ infrastructure/src/test/java/com/nesrux/catalogo/AbstractElasticSearchTest.java
@@ -0,0 +1,37 @@
+package com.nesrux.catalogo;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.elasticsearch.DataElasticsearchTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@ActiveProfiles("test-integration")
+@ComponentScan(
+  basePackages = "com.nesrux.catalogo",
+  useDefaultFilters = false,
+  includeFilters = {
+    @ComponentScan.Filter(type = FilterType.REGEX, pattern = "*.ElasticsearchGateway"),
+  }
+)
+@DataElasticsearchTest
+@ImportTestcontainers(ElasticSearchTestContainer.class)
+@Testcontainers
+@Tag("integrationTest")
+public abstract class AbstractElasticSearchTest {
+
+  @Autowired
+  private Collection<ElasticsearchRepository<?, ?>> repositories;
+
+  @BeforeEach
+  void cleanUp() {
+    this.repositories.forEach(ElasticsearchRepository::deleteAll);
+  }
+}
