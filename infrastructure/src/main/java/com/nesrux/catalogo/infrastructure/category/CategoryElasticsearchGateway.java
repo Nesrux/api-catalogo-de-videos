e5d567f8b80ffa8e4e78f60657da78129483f501
Nesrux <joaomarcosdevs@gmail.com>
2024-08-16 18:39:25 -0300

    fix: resolvi ?

--- infrastructure/src/main/java/com/nesrux/catalogo/infrastructure/category/CategoryElasticsearchGateway.java
+++ infrastructure/src/main/java/com/nesrux/catalogo/infrastructure/category/CategoryElasticsearchGateway.java
@@ -1,29 +1,26 @@
 package com.nesrux.catalogo.infrastructure.category;
 
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+import org.xnio.channels.UnsupportedOptionException;
+
 import com.nesrux.catalogo.domain.category.Category;
 import com.nesrux.catalogo.domain.category.CategoryGateway;
 import com.nesrux.catalogo.domain.category.CategorySearchQuery;
 import com.nesrux.catalogo.domain.pagination.Pagination;
 import com.nesrux.catalogo.infrastructure.category.models.persistence.CategoryDocoument;
 import com.nesrux.catalogo.infrastructure.category.models.persistence.CategoryRepository;
-import java.util.Objects;
-import java.util.Optional;
-import org.springframework.data.elasticsearch.core.SearchOperations;
-import org.springframework.stereotype.Component;
-import org.xnio.channels.UnsupportedOptionException;
 
 @Component
 public class CategoryElasticsearchGateway implements CategoryGateway {
 
    private final CategoryRepository repository;
-   private final SearchOperations searchOperations;
 
-   public CategoryElasticsearchGateway(
-      final CategoryRepository repository,
-      final SearchOperations searchOperations
-   ) {
+
+   public CategoryElasticsearchGateway(final CategoryRepository repository) {
       this.repository = Objects.requireNonNull(repository);
-      this.searchOperations = Objects.requireNonNull(searchOperations);
    }
 
    @Override
@@ -34,7 +31,9 @@ public class CategoryElasticsearchGateway implements CategoryGateway {
 
    @Override
    public void deleteById(final String anId) {
-      throw new UnsupportedOptionException();
+      if (repository.existsById(anId)) {
+         repository.deleteById(anId);
+      }
    }
 
    @Override
--- infrastructure/src/test/java/com/nesrux/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.java
+++ infrastructure/src/test/java/com/nesrux/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.java
@@ -1,14 +1,13 @@
 package com.nesrux.catalogo.infrastructure.category;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.nesrux.catalogo.AbstractElasticSearchTest;
 import com.nesrux.catalogo.domain.Fixture;
 import com.nesrux.catalogo.domain.utils.IdUtils;
 import com.nesrux.catalogo.infrastructure.category.models.persistence.CategoryDocoument;
 import com.nesrux.catalogo.infrastructure.category.models.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CategoryElasticsearchGatewayTest extends AbstractElasticSearchTest {
 
@@ -69,7 +68,7 @@ public class CategoryElasticsearchGatewayTest extends AbstractElasticSearchTest 
       final var expectedCount = 1;
 
       categoryRepository.save(CategoryDocoument.from(aCategory));
-      Assertions.assertTrue(categoryRepository.existsById(expectedId));
+      Assertions.assertTrue(categoryRepository.existsById(aCategory.id()));
 
       //when
       Assertions.assertDoesNotThrow(() -> categoryGateway.deleteById(expectedId));
