5f13a5df4dd8e1d509435e07879c9826cbb6b353
Nesrux <joaomarcosdevs@gmail.com>
2024-09-02 17:57:53 -0300

    feat: adiciona novos comandos ao run.sh

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,7 +1,13 @@
 # Criar as docker networks
 docker network create elastic
+docker network create kafka
+
 
 #Criar os docker volumes
 docker volume create es01
+docker volume create kafka01
+docker volume create kconnect01
 
 docker compose -f elk/docker-compose.yml up -d elasticsearch
+docker compose -f kafka/docker-compose.yml up -d
+docker compose -f catalogo_services/docker-compose.yml up -d
